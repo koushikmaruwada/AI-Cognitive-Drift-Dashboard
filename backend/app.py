47b25c46fdb2f387b8bf6e44b58a4be148cc68bc
MARUWADA KOUSHIK <maruwadakoushik@gmail.com>
2026-05-06 21:37:21 +0530

    Fixed Render deployment

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 from routes.analytics import analytics_bp
 from routes.upload import upload_bp
@@ -16,4 +17,12 @@ def home():
     return "AI Cognitive Drift Backend Running"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    port = int(
+        os.environ.get("PORT", 5000)
+    )
+
+    app.run(
+        host='0.0.0.0',
+        port=port
+    )

b2909bfcdf1d5074ba69ae740f8e3d2381f23a15
MARUWADA KOUSHIK <maruwadakoushik@gmail.com>
2026-05-12 22:16:31 +0530

    Fixed Twilio security

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from flask_cors import CORS
 import os
+
 from routes.mobile_data import mobile_bp
 from routes.analytics import analytics_bp
 from routes.upload import upload_bp
+
 from twilio.rest import Client
+
 app = Flask(__name__)
 
 CORS(app)
+
+# =========================
 # Twilio Setup
+# =========================
 
-account_sid = "YOUR_SID"
+account_sid = os.environ.get(
+    "TWILIO_ACCOUNT_SID"
+)
 
-auth_token = "YOUR_TOKEN"
+auth_token = os.environ.get(
+    "TWILIO_AUTH_TOKEN"
+)
 
 client = Client(
     account_sid,
@@ -25,33 +35,64 @@ def send_parent_alert(message):
 
         body=message,
 
-        from_="+1XXXXXXXXXX",
+        from_="+99550685918",
 
-        to="+91PARENTNUMBER"
+        to="+919550685918"
     )
-app.register_blueprint(analytics_bp)
-app.register_blueprint(upload_bp)
-app.register_blueprint(mobile_bp)
+
+# =========================
+# Blueprints
+# =========================
+
+app.register_blueprint(
+    analytics_bp
+)
+
+app.register_blueprint(
+    upload_bp
+)
+
+app.register_blueprint(
+    mobile_bp
+)
+
+# =========================
+# Routes
+# =========================
+
 @app.route('/')
+
 def home():
-    @app.route('/test-alert')
+
+    return "AI Cognitive Drift Backend Running"
+
+@app.route('/test-alert')
 
 def test_alert():
 
     send_parent_alert(
+
         "Student distracted during Zoom class"
     )
 
-    return "Alert Sent"
-    return "AI Cognitive Drift Backend Running"
+    return "Alert Sent Successfully"
+
+# =========================
+# Run App
+# =========================
 
 if __name__ == '__main__':
 
     port = int(
-        os.environ.get("PORT", 5000)
+        os.environ.get(
+            "PORT",
+            5000
+        )
     )
 
     app.run(
+
         host='0.0.0.0',
+
         port=port
     )
